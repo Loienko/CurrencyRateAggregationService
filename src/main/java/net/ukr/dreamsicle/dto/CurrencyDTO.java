3505a97166870405a5de399b103649ae41ef3ba0
yurii.loienko <dreamsicle@ukr.net>
2019-09-12 17:57:50 +0300

    CURR-42_Change a little CurrencyCodeValidator

--- src/main/java/net/ukr/dreamsicle/dto/CurrencyDTO.java
+++ src/main/java/net/ukr/dreamsicle/dto/CurrencyDTO.java
@@ -5,42 +5,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.ukr.dreamsicle.validation.ValidCurrencyCode;
 
-<<<<<<< HEAD
-import javax.validation.constraints.*;
-=======
 import javax.validation.constraints.NotBlank;
->>>>>>> origin/dev
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CurrencyDTO {
-<<<<<<< HEAD
-    @NotNull
+
+    @NotBlank(message = "Please fill the bank name")
     @Pattern(regexp = "^[a-zA-Zа-яёА-ЯЁ\\s\\-]+$")
     private String bankName;
 
-    @NotNull
+    @NotBlank(message = "Please fill the currency code")
     @ValidCurrencyCode
     private String currencyCode;
 
-    @NotNull
-    @Pattern(regexp = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\\.[0-9]{1,5}$")
-    private String purchaseCurrency;
-
-    @NotNull
-    @Pattern(regexp = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\\.[0-9]{1,5}$")
-=======
-    @NotBlank(message = "Please fill the bank name")
-    private String bankName;
-
-    @NotBlank(message = "Please fill the currency code")
-    private String currencyCode;
-
     @NotBlank(message = "Please fill the purchase currency")
+    @Pattern(regexp = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\\.[0-9]{1,5}$")
     private String purchaseCurrency;
 
     @NotBlank(message = "Please fill the sale of currency")
->>>>>>> origin/dev
+    @Pattern(regexp = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\\.[0-9]{1,5}$")
     private String saleOfCurrency;
 }
