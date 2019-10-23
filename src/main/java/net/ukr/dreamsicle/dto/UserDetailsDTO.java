5b8aba530f3f9f37d26a090fa9314cc6c229b070
yurii.loienko <dreamsicle@ukr.net>
2019-10-23 17:01:42 +0300

    feature_CURR-89_Add_swagger_documentation

--- src/main/java/net/ukr/dreamsicle/dto/UserDetailsDTO.java
+++ src/main/java/net/ukr/dreamsicle/dto/UserDetailsDTO.java
@@ -1,5 +1,7 @@
 package net.ukr.dreamsicle.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,16 +14,20 @@ import javax.validation.constraints.Size;
 @Data
 @Builder
 @NoArgsConstructor
+@ApiModel(description = "A class representing additional data for the user.")
 @AllArgsConstructor
 public class UserDetailsDTO {
 
+    @ApiModelProperty(notes = "User surname", example = "Surname", required = true)
     @NotBlank(message = "Please fill the surname")
     @Pattern(regexp = "^[a-zA-Zа-яёА-ЯЁ\\s\\-]+$", message = "Please input valid data for surname")
     private String surname;
 
+    @ApiModelProperty(notes = "User phone number", example = "+38(012)345-67-89")
     @Pattern(regexp = "^\\+?([0-9]{2})?\\(?[0-9]{3}\\)?[0-9]{3}\\-?[0-9]{2}\\-?[0-9]{2}$", message = "Please input valid data for phone")
     private String phone;
 
+    @ApiModelProperty(notes = "User description", example = "Good Person!", required = true)
     @Size(max = 256)
     @NotBlank(message = "Please fill the description")
     @Pattern(regexp = "^[a-zA-Zа-яёА-ЯЁ\\s\\-]+$", message = "Please input valid data for description")
--- src/main/java/net/ukr/dreamsicle/dto/UsernameAndPasswordDataDTO.java
+++ src/main/java/net/ukr/dreamsicle/dto/UsernameAndPasswordDataDTO.java
@@ -1,5 +1,7 @@
 package net.ukr.dreamsicle.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +14,16 @@ import javax.validation.constraints.Pattern;
 @Data
 @Builder
 @AllArgsConstructor
+@ApiModel(description = "A class representing the username and password for accessing the program functionality.")
 @NoArgsConstructor
 public class UsernameAndPasswordDataDTO {
 
+    @ApiModelProperty(notes = "User name", example = "admin", required = true)
     @NotBlank(message = "Please fill the username")
     @Pattern(regexp = "^[a-zA-Zа-яёА-ЯЁ\\s\\-]+$", message = "Please input valid data for username")
     private String username;
 
+    @ApiModelProperty(notes = "Conditional word or set of signs intended to confirm identity or authority.", example = "Qwerty1", required = true)
     @NotBlank(message = "Please fill the password")
     @ValidPassword(message = "Please input valid data for password")
     private String password;
