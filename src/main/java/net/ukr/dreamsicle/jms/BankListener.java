ce8e1ba884228b39d600b9c2af51e69c22813673
yurii.loienko <dreamsicle@ukr.net>
2019-12-05 18:51:19 +0200

    feature_CURR-142_Corrected_review

--- src/main/java/net/ukr/dreamsicle/jms/BankListener.java
+++ src/main/java/net/ukr/dreamsicle/jms/BankListener.java
@@ -1,28 +1,28 @@
 package net.ukr.dreamsicle.jms;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import net.ukr.dreamsicle.dto.bank.BankDTO;
 import net.ukr.dreamsicle.dto.bank.BankMapper;
 import net.ukr.dreamsicle.dto.gbsr.BankData;
 import net.ukr.dreamsicle.dto.gbsr.BankDataMapper;
-import net.ukr.dreamsicle.repository.BankRepository;
 import net.ukr.dreamsicle.service.BankService;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @AllArgsConstructor
 @Component
 public class BankListener {
     private final BankService bankService;
     private final BankDataMapper bankDataMapper;
-    private final BankRepository bankRepository;
     private final BankMapper bankMapper;
 
     @JmsListener(destination = "${app.bankQueueServer}")
-    public void receive(String data) {
-        var bankDTO = bankDataMapper.bankDataToBankDto(new Gson().fromJson(data, BankData.class), BankDTO.builder().build());
-        bankRepository.findBankByBankCode(bankDTO.getBankCode()).ifPresentOrElse(
+    public void receive(String data) throws IOException {
+        BankDTO bankDTO = bankDataMapper.bankDataToBankDto(new ObjectMapper().readValue(data, BankData.class), BankDTO.builder().build());
+        bankService.bankDataByBankCode(bankDTO).ifPresentOrElse(
                 bank -> bankService.update(bank.getId(), bankMapper.toBankUpdateDTO(bankDTO)),
                 () -> bankService.create(bankDTO)
         );
